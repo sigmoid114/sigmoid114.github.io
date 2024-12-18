7aae683e8586872185ee03d5cd8a7ade19c465e7
sigmoid114 <2381542738@qq.com>
2024-12-18 23:14:13 +0800

    delete some bugs

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,13 +1,15 @@
 const name="sigmoid114 的博客";
 const intr="#define inf 1145141919";
 
-document.title+=" "+name;
-div=document.getElementsByClassName('name');
-for(var i=0,n=div.length;i<n;i+=1){
-    div[i].innerHTML=name;
-}
+function load_main() {
+    document.title += " " + name;
+    div = document.getElementsByClassName('name');
+    for (var i = 0, n = div.length; i < n; i += 1) {
+        div[i].innerHTML = name;
+    }
 
-div=document.getElementsByClassName('intr');
-for(var i=0,n=div.length;i<n;i+=1){
-    div[i].innerHTML=intr;
+    div = document.getElementsByClassName('intr');
+    for (var i = 0, n = div.length; i < n; i += 1) {
+        div[i].innerHTML = intr;
+    }
 }
