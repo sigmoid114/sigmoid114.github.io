e2f7bacf319c5a35df6b617653adae3b589e28d7
sigmoid114 <2381542738@qq.com>
2024-12-22 16:07:06 +0800

    style of code and some other things

--- blog/js/loadtags.js
+++ blog/js/loadtags.js
@@ -12,12 +12,14 @@ fetch('../json/tags.json')
     .then(dat => {
         list=JSON.parse(dat).list;
         n=list.length;
-        div=document.getElementById('list');
+        ul=document.getElementById('list');
         for(var i=0;i<n;i+=1){
             a=document.createElement('a');
             a.href='./'+list[i].id+'.html';
             a.innerHTML=list[i].name;
-            div.appendChild(a);
+            li=document.createElement('li');
+            li.appendChild(a);
+            ul.appendChild(li);
         }
     })
     .catch(err => console.log('Error:',err))
